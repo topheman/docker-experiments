9801673784be3090d43c73a120ca6deb0af2e69c
Christophe Rosset <tophe@topheman.com>
2018-07-15 22:47:45 +0200

    feat(api/home): add basic infos on root page in html/json

--- api/main.go
+++ api/main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"runtime"
 
 	"github.com/gorilla/mux"
 )
 
-func helloWorld(w http.ResponseWriter, _ *http.Request) {
+type infos struct {
+	Hostname string `json:"hostname"`
+	Version  string `json:"version"`
+	Cpus     int    `json:"cpus"`
+}
+
+func makeInfos() *infos {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "Unknown"
+	}
+	return &infos{hostname, runtime.Version(), runtime.NumCPU()}
+}
+
+func rootRouteHTML(w http.ResponseWriter, _ *http.Request) {
+	infos := makeInfos()
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Hello World</h1>")
+	fmt.Fprintf(w, `<h1>Welcome</h1>
+	<ul>
+		<li>Hostname: %s</li>
+		<li>Version: %s</li>
+		<li>Number of CPUs: %d</li>
+	</ul>`, infos.Hostname, infos.Version, infos.Cpus)
+}
+
+func rootRouteJSON(w http.ResponseWriter, r *http.Request) {
+	infos := makeInfos()
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(infos)
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", helloWorld)
+	router.HandleFunc("/", rootRouteJSON).Methods("GET").HeadersRegexp("Accept", "application/json")
+	router.HandleFunc("/", rootRouteJSON).Methods("GET").Queries("format", "json")
+	router.HandleFunc("/", rootRouteHTML).Methods("GET")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
