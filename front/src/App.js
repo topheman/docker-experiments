38f10ace42fc93b2c9fc5622e66a4b50cd3c74de
Christophe Rosset <tophe@topheman.com>
2018-07-16 11:40:36 +0200

    feat(front/home): display basic api infos on home page + logos
    Note: Using docker networks AND create-react-app proxy

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import reactLogo from './assets/images/react-logo.svg';
+import dockerLogo from './assets/images/docker-logo.svg';
+import gopher from './assets/images/gopher.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    loading: false,
+    error: null,
+    data: null
+  }
+  componentDidMount() {
+    this.fetchInfos()
+  }
+  fetchInfos() {
+    this.setState({
+      error: null,
+      loading: true,
+      data: null
+    });
+    axios.get('/api').then(result => {
+      this.setState({
+        data: result.data,
+        loading: false
+      })
+    })
+    .catch(error => {
+      this.setState({
+        error,
+        loading: false
+      })
+    })
+  }
   render() {
+    const {loading, error, data} = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={dockerLogo} className="App-logo" alt="logo" />
+          <img src={reactLogo} className="App-logo" alt="logo" />
+          <img src={gopher} style={{marginLeft: 12}} className="App-logo" alt="logo" />
+          <h1 className="App-title">my-docker-fullstack-project</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Basic infos retrieved from the dockerized golang api:</p>
+        {loading && <p>Loading ...</p>}
+        {error && <p>An error occured</p>}
+        {data && <ul style={{listStyle: "none", paddingLeft: 0}}>{Object.entries(data).map(([key, value]) => <li key={key}>
+          <strong>{key}</strong>: <span>{value}</span>
+        </li>)}</ul>}
+        <p><button onClick={() => this.fetchInfos()}>Reload</button></p>
       </div>
     );
   }
